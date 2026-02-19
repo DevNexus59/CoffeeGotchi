930ede579469fbd0702addb94685f12da9b7e51c
Pierre Fourdin <fourdin.pfmg@gmail.com>
2026-02-19 14:33:20 +0100

    add types for animalstat,remedy,animaltypes

---
+++ src/types/game.ts
@@ -0,0 +1,3 @@
+export type AnimalState = "normal" | "coffee" | "stone" | "dead";
+export type Remedy = "coffee" | "herbaltea";
+export type AnimalType = "tiger" | "chicken" | "fox" | "elephant" | "turtle" | "penguin";
