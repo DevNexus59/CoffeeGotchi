abaaa80867e804fcc4168b841f33c0e9167f0ac2
Romain Debas <debasromain@gmail.com>
2026-02-19 14:32:59 +0100

    editing the loader screen, and adding with the assets, and adding it to app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Loader from './src/screens/Loader';
 
 export default function App() {
+
+  //Création d'un état pour savoir si on affiche le loader ou autre page
+  const [isLoading, setIsLoading] = useState(true)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {isLoading ? (
+        <Loader onFinished={() => setIsLoading(false)} />
+      ) : (
+        // Remplacer ce bloc ensuite
+        // par le SelectionScreen
+        <View style={styles.gameContent}>
+      <Text>CoffeeGotchi</Text>
+      </View>
+      )}
     </View>
   );
 }
@@ -13,8 +27,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  gameContent: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF5E1',
   },
 });
